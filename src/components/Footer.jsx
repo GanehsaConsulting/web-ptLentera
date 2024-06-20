f39a4c2f1ea98e75217792efe496e08d1a7530c6
preettypeanuts <mrafly212001@gmail.com>
2024-06-20 22:05:47 +0700

    f

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export const Footer = () => {
     return (
         <>
-            <footer className="footer p-10 bg-base-200 text-base-content">
+            <footer className="footer p-10 bg-base-200 text-base-content mt-10">
                 <aside>
                     <Image
                         width={100}
